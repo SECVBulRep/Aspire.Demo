d48ea64516e59c6b4109883067203cec9ea33325
bulat <farukshin@molot.ru>
2024-01-17 18:55:47 +0300

    * пример проекта

---
+++ Web/Clients/WeatherApiClient.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.Clients;
+
+public class WeatherApiClient(HttpClient client)
+{
+    public async Task<WeatherForecast[]> GetForecast() =>
+        await client.GetFromJsonAsync<WeatherForecast[]>("weatherforecast") ?? [];
+}
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
