22512c32e9f3a3b4e20c66340243b6471f4dc82b
HOME-PC\Bulat <bfarukshin@gmail.com>
2024-01-24 19:23:49 +0300

    добавил пример  с pg

---
+++ Web/Db/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web.Db;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+        
+        Database.EnsureDeleted();   // удаляем бд со старой схемой
+        Database.EnsureCreated();   // создаем бд с новой схемой
+    }
+}
